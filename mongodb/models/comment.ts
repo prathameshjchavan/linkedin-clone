04b275f454357788403e18763428a24d74dd6480
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2024-05-17 19:13:34 +0530

    create comment model and schema

---
+++ mongodb/models/comment.ts
@@ -0,0 +1,28 @@
+import { User } from "@/types/user";
+import mongoose, { Schema, Document, models } from "mongoose";
+
+export interface ICommentBase {
+	user: User;
+	text: string;
+}
+
+export interface IComment extends Document, ICommentBase {
+	createdAt: Date;
+	updatedAt: Date;
+}
+
+const CommentSchema = new Schema<IComment>(
+	{
+		user: {
+			userId: { type: String, required: true },
+			userImage: { type: String, required: true },
+			firstName: { type: String, required: true },
+			lastName: { type: String },
+		},
+		text: { type: String, required: true },
+	},
+	{ timestamps: true }
+);
+
+export const Comment =
+	models.Comment || mongoose.model<IComment>("Comment", CommentSchema);
