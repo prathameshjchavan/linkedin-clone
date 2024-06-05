2f7b8c631a48f0d4bc9082794ac2d43e24a68004
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2024-06-05 11:10:56 +0530

    generateSASToken function

---
+++ lib/generateSASToken.ts
@@ -0,0 +1,44 @@
+import {
+	BlobSASPermissions,
+	BlobServiceClient,
+	StorageSharedKeyCredential,
+	generateBlobSASQueryParameters,
+} from "@azure/storage-blob";
+
+export const containerName = "posts";
+
+const accountName = process.env.AZURE_STORAGE_ACCOUNT_NAME;
+const accountKey = process.env.AZURE_STORAGE_ACCOUNT_KEY;
+
+if (!accountName || !accountKey)
+	throw new Error("Azure Storage account name and key are required");
+
+const sharedKeyCredential = new StorageSharedKeyCredential(
+	accountName,
+	accountKey
+);
+
+const blobServiceClient = new BlobServiceClient(
+	`https://${accountName}.blob.core.windows.net`,
+	sharedKeyCredential
+);
+
+async function generateSASToken() {
+	const containerClient = blobServiceClient.getContainerClient(containerName);
+
+	const expiryDate = new Date();
+	expiryDate.setMinutes(expiryDate.getMinutes() + 30);
+
+	const sasToken = generateBlobSASQueryParameters(
+		{
+			containerName,
+			permissions: BlobSASPermissions.parse("rcw"),
+			expiresOn: expiryDate,
+		},
+		sharedKeyCredential
+	).toString();
+
+	return sasToken;
+}
+
+export default generateSASToken;
